7adb5372cfd82b385df0b2a857dd8da538590349
mstf-yalcin <mstf.yalcin@outlook.com>
2024-02-24 11:48:58 +0300

    Added extension methods for checking if an IP address is IPv4 or IPv6.

--- src/Core.QuickExtend/Extensions/IPAddress/IPAddressExtensions.cs
+++ src/Core.QuickExtend/Extensions/IPAddress/IPAddressExtensions.cs
@@ -1,3 +1,28 @@
 ﻿namespace Core.QuickExtend.Extensions;
 
-public static partial class IPAddressExtensions { }
+public static partial class IPAddressExtensions {
+
+        public static bool IsIPv4Address(this IPAddress ipAddress)
+        {
+            return ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork;
+        }
+
+        public static bool IsIPv4Address(this string input)
+        {
+            IPAddress ipAddress;
+            return IPAddress.TryParse(input, out ipAddress) && ipAddress.IsIPv4Address();
+        }
+
+        public static bool IsIPv6Address(this IPAddress ipAddress)
+        {
+            return ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6;
+        }
+
+        public static bool IsIPv6Address(this string input)
+        {
+            IPAddress ipAddress;
+            return IPAddress.TryParse(input, out ipAddress) && ipAddress.IsIPv6Address();
+        }
+
+
+ }
